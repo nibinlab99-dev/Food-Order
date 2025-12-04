6f1be32f6b29fe67010a2103aad4abbccb8ff6e9
nibin-lab <nibin.lab.99@gmail.com>
2025-12-04 15:31:15 +0530

    feat: add dynamic header with navigation

---
+++ app/menu/page.tsx
@@ -0,0 +1,8 @@
+export default function MenuPage() {
+    return (
+        <div className="max-w-7xl m-auto py-6">
+            <h1 className="text-2xl font-bold">Our Menu</h1>
+            <p>Here you will see all our delicious food items.</p>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h2>Test</h2>
+    <div className="max-w-7xl m-auto py-6">
+      <h2 className="text-2xl font-bold">Home Page</h2>
+    </div>
   );
 }
